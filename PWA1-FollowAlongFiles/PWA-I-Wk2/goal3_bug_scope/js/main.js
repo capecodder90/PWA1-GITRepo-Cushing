110508164e0e1c4b82357fecd8f399728c98ffe1
capecodder90 <scushing90@comcast.net>
2014-12-02 19:46:51 -0500

    Followed along with videos on debugging
    Followed along with videos on debugging

--- PWA1-FollowAlongFiles/PWA-I-Wk2/goal3_bug_scope/js/main.js
+++ PWA1-FollowAlongFiles/PWA-I-Wk2/goal3_bug_scope/js/main.js
@@ -87,89 +87,89 @@ console.log('------ Debugging ----------');
      display.  You may want to see the error messages in Chrome and Firefox.
  ***********************/
 
- /*
+
 	//Example1
-	var name = james, course = 'PWA1, month = 3;
-	console.log ("name: ", name + " / course: ", coures + " / month: ", month)
+	//var name = 'james', course = 'PWA1', month = 3;
+	//console.log ("name: ", name + " / course: ", course + " / month: ", month)
 
 
     //Example2:
-	//string w/ escape charaters and mixed single/double quotes
-	var phrase = 'he's in ' + ' "PWA1' ";
-	console.log("phase 4: ", phr@se);
+	//string w/ escape characters and mixed single/double quotes
+	//var phrase = 'he\'s in ' + '"PWA1"';
+	//console.log("phase 4: ", phrase);
 
 
     //Example3:
 	//nested conditional statement
-	if (a === a){    //1st IF statement
-		//execute this block of code if a is equal to a
-		
-		if (b === b){  //nested IF statement : 2nd IF statement
-   			//execute this block of code if b is equal to b
-   			console.log("In Nested if-else / b === b: True");
-		}else{       //nested ELSE statement
-			//execute block of code if the matching “IF” statement returns false
-		};
-
-	}else
-		//execute this block of code if the 1st “IF” statement returns false
-		console.log("nested conditional: 1st IF returned false);
-	};
+//	if ("a" === "a"){    //1st IF statement
+//		//execute this block of code if a is equal to a
+//
+//		if ("b" === "b"){  //nested IF statement : 2nd IF statement
+//   			//execute this block of code if b is equal to b
+//   			console.log("In Nested if-else / b === b: True");
+//		}else{       //nested ELSE statement
+//			//execute block of code if the matching “IF” statement returns false
+//		};
+//
+//	}else{
+//		//execute this block of code if the 1st “IF” statement returns false
+//		console.log("nested conditional: 1st IF returned false");
+//	};
 
 
     //Example4:
-	var value1 = 'Sunny';
-	var value2 = 10;
-	
-	var mood = function(weather, waves)
-
-		if (weather = "Sunny"){
-			if (waves === 10){
-				Mood = 'PUMPED';
-			}else if ((wave < 9) && (waves >= 5)){
-				Mood = 'mellow';			
-			}else{
-				Mood = 'bummed';
-			};	
-	
-		}else if (weather === 'Overcast'){
-			if ((wave <= 10) !! (waves >= 7)){
-				Mood = "JACKED UP";
-			}else if ((waves <= 6) && (waves >= 3)){
-				Mood = 'totally bummed';			
-			)else{
-				Mood = 'not happy';
-			};
-
-		}else{
-			Mood = 'sad'
-		};
-
-		return Moood;
-	};
-
-	var moodType = mood(value1, value2);
-	console.log('mood: ', moodType);
+//	var value1 = 'Sunny';
+//	var value2 = 10;
+//
+//	var mood = function(weather, waves){
+//
+//		if (weather = "Sunny"){
+//			if (waves === 10){
+//				Mood = 'PUMPED';
+//			}else if ((wave < 9) && (waves >= 5)){
+//				Mood = 'mellow';
+//			}else{
+//				Mood = 'bummed';
+//			};
+//
+//		}else if (weather === 'Overcast'){
+//			if ((wave <= 10) || (waves >= 7)){
+//				Mood = "JACKED UP";
+//			}else if ((waves <= 6) && (waves >= 3)) {
+//                Mood = 'totally bummed';
+//            }else{
+//				Mood = 'not happy';
+//			};
+//
+//		}else{
+//			Mood = 'sad'
+//		};
+//
+//		return Mood;
+//	};
+//
+//	var moodType = mood(value1, value2);
+//	console.log('mood: ', moodType);
 
 
     //Example5:
 
-    var myNums = [1, 2, 3, 4, 5];
-    console.log(myNums);
-
-    for (var i=10, j=myNums.length; i < j; i++){
-
-        console.log("i: " + i);
-        console.log("j: " + j);
-
-        if (i === 3) {
-            console.log("testing out the break and it broke");
-        };
-
-        continue;
-        console.log('What is at index ' + i + ' = ', myNums[i] );
-    };
-
+//    var myNums = [1, 2, 3, 4, 5];
+//    console.log(myNums);
+//
+//    for (var i=0, j=myNums.length; i < j; i++){
+//
+//        console.log("i: " + i);
+//        console.log("j: " + j);
+//
+//        if (i === 3) {
+//            console.log("testing out the break and it broke");
+//            continue;
+//        };
+//
+//        console.log('What is at index ' + i + ' = ', myNums[i] );
+//    };
+    /*
 *****************/
 
 /*
@@ -195,6 +195,25 @@ console.log('------ Debugging ----------');
   		}
 */
     console.log('------ Try Catch ----------');
+
+    var return10 = function(){
+        return 15;
+    };
+
+    var num = return10();
+
+    try
+    {
+        if( num === "") throw "empty";
+        if( isNaN(num)) throw "not a number";
+        if( num > 10) throw "too high";
+        if( num < 10) throw "too low";
+    }
+
+    catch(err)
+    {
+        console.log(err);
+    };
 
 
 /*
