d0e4f3a3154230f9b0e18834cb3c8bf04c72880c
capecodder90 <scushing90@comcast.net>
2014-12-17 10:59:12 -0500

    Write pseudo-code for Object 7 homework assign.
    Write pseudo-code for Object 7 homework assignment

---
+++ PWA1/Wk4/goal7_assign_objects/js/main.js
@@ -0,0 +1,27 @@
+/**
+ * Name: Sarah Cushing
+ * Date: December 17, 2014
+ * Class: PWA
+ * Goal: Goal7
+ */
+
+// declare global variables including:
+    // "names", array with names of 5 people
+    // "instances", maximum number of instances of the Person object (3)
+    // "people", empty array
+    // "interval", empty variable
+
+// create functions including:
+    // "populateHTML",
+        // use innerHTML to inject the names and jobs of the people
+    // "runUpdate",
+        // create "update" function to loop through each Person instance stored in the People array
+
+// create a for loop (using "instances" for the comparison)
+    // create a random number between 1 and 5 (number of names in array) using Math.random, store it in the variable "someName"
+    // create Person object (var person = new Person(names[someName], someRow)), store in "people" array
+    // call "populateHTML" function
+    // don't allow duplicate names to appear on the web page
+
+// call "runUpdate" function, set to run 30 times per second
+
---
+++ PWA1/Wk4/goal7_assign_objects/js/person.js
@@ -0,0 +1,22 @@
+/**
+ * Name: Sarah Cushing
+ * Date: December 17, 2014
+ * Class: PWA
+ * Goal: Goal7
+ */
+
+// add Person object to the global window (window.Person = Person;)
+
+// declare variables including:
+    // "jobs", array of 4 or more jobs
+    // "actions", array of actions a person can do
+
+// create Person constructor function with:
+    // "name", this.name sets the name of the person
+    // "action", this.action randomly sets the action using Math.random
+    // "job", this.job randomly sets the job using Math.random
+    // "row", this.row sets the row
+
+    // set initial action
+
+// use "update" function from main.js file to randomly change the action of the person and write that change to column 3
